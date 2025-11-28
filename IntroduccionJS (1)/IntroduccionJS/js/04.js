9f0acdb649c65598cd41a14ce7c0d29511a4f823
Elias3031 <jperegrina4@ucol.mx>
2025-11-28 09:26:25 -0600

    Add files via upload

---
+++ IntroduccionJS (1)/IntroduccionJS/js/04.js
@@ -0,0 +1,19 @@
+// String o Cadenas de Texto
+
+const mensaje = 'Aprendiendo JavaScript en Fundamentos de Programación';
+const producto2 = 'Monitor HD"';
+const nombre = 'Rodrigo Ramx';
+const email = 'rods@ucol.mx';
+
+// length es para la extension
+console.log(mensaje.length);
+console.log(producto2);
+
+// IndexOf (retorna posición)
+console.log(mensaje.indexOf('JavaScript'));
+console.log(producto2.indexOf('Tablet'));
+console.log(email.indexOf('@'));
+
+// Includes (retorna true o false)
+console.log(mensaje.includes('JavaScript'));
+console.log(producto2.includes('Tablet'));
---
+++ IntroduccionJS (1)/IntroduccionJS/js/08.js
@@ -0,0 +1,17 @@
+const nombre = 'Maximiliano Codificador';
+const email = 'max@ucol.mx';
+
+// Concatenación
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+//separador por comas
+console.log("Nombre Cliente:", nombre, "Email:", email);
+
+// Template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
+
+
+//impresion en el documento
+document.write(`Nombre Cliente: ${nombre} Email: ${email}`);    
+document.write('<h1>Hola Mundo</h1>');
+document.write('<p>Este es un párrafo</p>');
